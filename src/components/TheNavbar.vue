32e77d9f713fea735965cdd60ef0c99901c6c781
Dian Azizah <nive54@gmail.com>
2023-03-20 19:14:28 +0700

    first commit

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <header class="header" id="header"
+    v-click-outside="()=> mobileNavMenu=false"
+    v-page-scroll="()=> mobileNavMenu = false"
+  >
+
+    <router-link :to="{name: 'Home'}" class="logo">
+     <h1>
+        Forum
+     </h1>
+    </router-link>
+
+    <div class="btn-hamburger" @click="mobileNavMenu = !mobileNavMenu">
+      <!-- use .btn-humburger-active to open the menu -->
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+
+    <nav class="navbar" :class="{'navbar-open': mobileNavMenu}">
+      <ul>
+        <li v-if="authUser" class="navbar-user">
+          <a
+            @click.prevent="userDropdownOpen = !userDropdownOpen"
+            v-click-outside="()=> userDropdownOpen = false"
+          >
+            <AppAvatarImg class="avatar-small" :src="authUser.avatar || authUser.name.charAt(0)" :alt="`${authUser.name} profile picture`"/>
+            <span>
+                {{authUser.name}}
+                <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt=""/>
+            </span>
+          </a>
+
+          <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
+            <div class="triangle-drop"></div>
+            <ul class="dropdown-menu">
+              <li class="dropdown-menu-item"><router-link :to="{name: 'Profile'}">View profile</router-link></li>
+              <li class="dropdown-menu-item"><a
+                @click.prevent="$store.dispatch('auth/signOut'),
+                $router.push({name: 'Home'})">
+                Sign Out
+              </a></li>
+            </ul>
+          </div>
+        </li>
+        <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'SignIn'}">Sign In</router-link></li>
+        <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'Register'}">Register</router-link></li>
+        <li v-if="authUser" class="navbar-mobile-item"><router-link :to="{name: 'Profile'}">View Profile</router-link></li>
+        <li v-if="authUser" class="navbar-mobile-item"><a
+          @click.prevent="$store.dispatch('auth/signOut'),
+          $router.push({name: 'Home'})">
+        Sign Out</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  data () {
+    return {
+      userDropdownOpen: false,
+      mobileNavMenu: false
+    }
+  },
+  computed: {
+    ...mapGetters('auth', ['authUser'])
+  },
+  created () {
+    this.$router.beforeEach((to, from) => {
+      this.mobileNavMenu = false
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,148 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { findById } from '@/helpers'
+import store from '@/store'
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import ( /* webpackChunkName: "Home" */ '@/pages/Home'),
+        meta: {
+            title: 'Home'
+        },
+    },
+    {
+        path: '/me',
+        name: 'Profile',
+        component: () =>
+            import ( /* webpackChunkName: "Profile" */ '@/pages/Profile'),
+        meta: { title: 'Profile', toTop: true, smoothScroll: true, requiresAuth: true }
+    },
+    {
+        path: '/me/edit',
+        name: 'ProfileEdit',
+        component: () =>
+            import ( /* webpackChunkName: "Profile" */ '@/pages/Profile'),
+        props: { edit: true },
+        meta: { title: 'Profile Edit', requiresAuth: true }
+    },
+    {
+        path: '/category/:id',
+        name: 'Category',
+        component: () =>
+            import ( /* webpackChunkName: "Category" */ '@/pages/Category'),
+        props: true,
+        meta: {
+            title: 'Category'
+        }
+    },
+    {
+        path: '/forum/:id',
+        name: 'Forum',
+        component: () =>
+            import ( /* webpackChunkName: "Forum" */ '@/pages/Forum'),
+        props: true,
+        meta: {
+            title: 'Category'
+        }
+    },
+    {
+        path: '/thread/:id',
+        name: 'ThreadShow',
+        component: () =>
+            import ( /* webpackChunkName: "ThreadShow" */ '@/pages/ThreadShow'),
+        props: true,
+        async beforeEnter(to, from, next) {
+            await store.dispatch('threads/fetchThread', { id: to.params.id, once: true })
+                // check if thread exists
+            const threadExists = findById(store.state.threads.items, to.params.id)
+                // if exists continue
+            if (threadExists) {
+                return next()
+            } else {
+                next({
+                    name: 'NotFound',
+                    params: { pathMatch: to.path.substring(1).split('/') },
+                    // preserve existing query and hash
+                    query: to.query,
+                    hash: to.hash
+                })
+            }
+            // if doesnt exist redirect to not found
+        }
+    },
+    {
+        path: '/forum/:forumId/thread/create',
+        name: 'ThreadCreate',
+        component: () =>
+            import ( /* webpackChunkName: "ThreadCreate" */ '@/pages/ThreadCreate'),
+        props: true,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/thread/:id/edit',
+        name: 'ThreadEdit',
+        component: () =>
+            import ( /* webpackChunkName: "ThreadEdit" */ '@/pages/ThreadEdit'),
+        props: true,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () =>
+            import ( /* webpackChunkName: "Register" */ '@/pages/Register'),
+        meta: { title: 'Register', requiresGuest: true }
+    },
+    {
+        path: '/signin',
+        name: 'SignIn',
+        component: () =>
+            import ( /* webpackChunkName: "SignIn" */ '@/pages/SignIn'),
+        meta: { title: 'Signin', requiresGuest: true }
+    },
+    {
+        path: '/logout',
+        name: 'SignOut',
+        async beforeEnter(to, from) {
+            await store.dispatch('auth/signOut')
+            return { name: 'Home' }
+        }
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () =>
+            import ( /* webpackChunkName: "NotFound" */ '@/pages/NotFound'),
+        meta: {
+            title: 'Not-Found'
+        }
+    }
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(to) {
+        const scroll = {}
+        if (to.meta.toTop) scroll.top = 0
+        if (to.meta.smoothScroll) scroll.behavior = 'smooth'
+        return scroll
+    }
+})
+router.afterEach(() => {
+    store.dispatch('clearItems', { modules: ['categories', 'forums', 'posts', 'threads'] })
+})
+
+router.beforeEach(async(to, from) => {
+    await store.dispatch('auth/initAuthentication')
+    store.dispatch('unsubscribeAllSnapshots')
+    if (to.meta.requiresAuth && !store.state.auth.authId) {
+        return { name: 'SignIn', query: { redirectTo: to.path } }
+    }
+    if (to.meta.requiresGuest && store.state.auth.authId) {
+        return { name: 'Home' }
+    }
+
+    document.title = `${to.meta.title} | Forum App`;
+})
+
+export default router
